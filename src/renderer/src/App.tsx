80346a210462e6639f7dd5f792e6435cb86e76e4
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-16 22:55:06 +0530

    Header

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,14 +1,17 @@
-import { Content, RootLayout, Sidebar } from "./components"
+import { Content, RootLayout, Sidebar, DragableTopBar } from "./components"
 
-function App() {
+const App = () => {
 	return (
 		// <div className="flex h-screen items-center justify-center">
 		// 	<span className="text-4xl text-cyan-500">Hello... Morningstar_2061...!</span>
 		// </div>
-		<RootLayout>
-			<Sidebar className="p-2">Sidebar</Sidebar>
-			<Content className="border-l bg-zinc-900/50 border-l-white/20">Contect</Content>
-		</RootLayout>
+		<>
+			<DragableTopBar />
+			<RootLayout>
+				<Sidebar className="p-2">Sidebar</Sidebar>
+				<Content className="border-l bg-zinc-900/50 border-l-white/20">Contect</Content>
+			</RootLayout>
+		</>
 	)
 }
 
---
+++ src/renderer/src/components/DraggableTopBar.tsx
@@ -0,0 +1,3 @@
+export const DragableTopBar = () => {
+    return <header className="absolute inset-0 h-8 bg-transparent border-4 border-red-500" />
+}
