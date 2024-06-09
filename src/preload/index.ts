8f3f6054ffa896184deed083f06b426df9b4b7fd
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-09 18:38:37 +0530

    ipc args

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,27 +1,5 @@
-import { contextBridge } from 'electron'
-/*import { electronAPI } from '@electron-toolkit/preload'
-
-
-// Custom APIs for renderer
-const api = {}
-
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-	try {
-		contextBridge.exposeInMainWorld('electron', electronAPI)
-		contextBridge.exposeInMainWorld('api', api)
-	} catch (error) {
-		console.error(error)
-	}
-} else {
-	// @ts-ignore (define in dts)
-	window.electron = electronAPI
-	// @ts-ignore (define in dts)
-	window.api = api
-}
-*/
+import { GetNotes } from '@shared/types'
+import { contextBridge, ipcRenderer } from 'electron'
 
 if (!process.contextIsolated) {
 	throw new Error('contextIsolation must be enabled in BrowserWindow')
@@ -29,7 +7,8 @@ if (!process.contextIsolated) {
 try {
 	contextBridge.exposeInMainWorld('context', {
 		locate: navigator.language,
-		platform: process.platform
+		platform: process.platform,
+		getNotes: (...args: Parameters<GetNotes>) => ipcRenderer.invoke('getNotes', ...args)
 	})
 } catch (error) {
 	console.log(error)
