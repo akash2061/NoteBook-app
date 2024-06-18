a446db11b8d543cb903142c0ff28bdfa0a805c5f
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-18 20:34:47 +0530

    Auto-Save

--- src/renderer/src/hooks/useMarkdownEditor.tsx
+++ src/renderer/src/hooks/useMarkdownEditor.tsx
@@ -1,10 +1,24 @@
-import { selectedNoteAtom } from "@renderer/store"
-import { useAtomValue } from "jotai"
+import { MDXEditorMethods } from "@mdxeditor/editor"
+import { saveNoteAtom, selectedNoteAtom } from "@renderer/store"
+import { NoteContent } from "@shared/models"
+import { useAtomValue, useSetAtom } from "jotai"
+import { useRef } from "react"
 
 export const useMarkdownEditor = () => {
     const selectedNote = useAtomValue(selectedNoteAtom)
+    const saveNote = useSetAtom(saveNoteAtom)
+    const editorRef = useRef<MDXEditorMethods>(null)
+
+    const handleAutoSave = async (content: NoteContent) => {
+        if (!selectedNote) return
+
+        console.log('Auto saveing:', selectedNote.title)
+        await saveNote(content)
+    }
 
     return {
-        selectedNote
+        editorRef,
+        selectedNote,
+        handleAutoSave,
     }
 }
