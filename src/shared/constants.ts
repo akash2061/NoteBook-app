89154dc6f0557949c1eaec1122b3968c6c70338a
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-21 14:55:44 +0530

    AutoSave Time Update

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const appDirectoryName = 'NoteBook'
 export const fileEncoding = 'UTF-8'
-export const autoSaveingTime = 3000
+export const autoSaveingTime = 1000
 export const welcomeNoteFilename = 'Welcome.md'
